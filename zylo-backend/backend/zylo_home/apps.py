7d9e034886dbafb1dec1404bf1192497ac265941
akshunyagcte0328 <akshunya.garg+gitlab@coriolis.co.in>
2025-01-09 23:02:45 +0530

    added the blank files for future implementation

--- zylo-backend/backend/zylo_home/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ZyloHomeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'zylo_home'
--- zylo-backend/backend/zylo_home/urls.py
+++
@@ -1,7 +0,0 @@
-# home/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home_view, name='home'),  # This maps the home_view to the root URL of this app
-]
