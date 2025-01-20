5ddd18d29ee449a36a09c43f8bcda12e1fe1eaec
Lakshya Raghuwanshi <lakshya.raghuwanshi@coriolis.co.in>
2025-01-20 22:06:14 +0530

    added model to admin

--- MediSync-backend/backend/accounts/admin.py
+++ MediSync-backend/backend/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models.user_model import User, AppStaff, Organization
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'user_type', 'email', 'organization')
+
+@admin.register(AppStaff)
+class AppStaffAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
