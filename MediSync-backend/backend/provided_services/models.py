b565df22c2f980acf13513e2727ba535df503c26
akshunyagcte0328 <akshunya.garg+gitlab@coriolis.co.in>
2025-01-25 22:25:37 +0530

    created a model which represents the hospitals all over

--- MediSync-backend/backend/provided_services/models.py
+++ MediSync-backend/backend/provided_services/models.py
@@ -11,3 +11,19 @@ class Disease(models.Model):
     def __str__(self):
         return self.name
 
+class Hospital(models.Model):
+    TYPE_CHOICES = [
+        ('hospital', 'Hospital'),
+        ('clinic', 'Clinic'),
+    ]
+    
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    location = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
+    contact_info = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
